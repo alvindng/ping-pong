b595714f4d563c7c9973a9dc103bc286f7dc63f2
Alvin Ng <alvindng@gmail.com>
2016-06-10 09:55:27 -0700

    add alert if user enters anything that is not a number

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#number").val());
+    if (invalidInput.test(userInput) === true) {
+      alert("Please enter numbers only")
+    } else {
     var numberOutput = userInput;
     $("#output").text(numberOutput);
+    }
   });
+
+  var invalidInput = /[^0-9]/;
+
 });
