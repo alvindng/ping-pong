1e45a0ba786267c7a8001d192485fb78de42a8c7
Alvin Ng <alvindng@gmail.com>
2016-06-10 11:12:42 -0700

    add consecutive count of the number entered by a user

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,19 @@ $(document).ready(function() {
     if (invalidInput.test(userInput) === true) {
       alert("Please enter numbers only")
     } else {
-    var numberOutput = userInput;
-    $("#output").text(numberOutput);
+    var numberOutput = numberConvert(userInput);
+    for (i=0; i<numberOutput.length; i++) {
+      $("#output").append("<li>" + numberOutput[i] + "</li>");
+    }
     }
   });
-
-  var invalidInput = /[^0-9]/;
-
 });
+
+  var numberConvert = function(number) {
+    var numberArray = [];
+    for (i =1; i <= number; i++){
+      numberArray.push(i);
+    }
+    return numberArray;
+  }
+    var invalidInput = /[^0-9]/;
